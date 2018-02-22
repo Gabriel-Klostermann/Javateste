3c44453b8a0c1ed3680a7b584c5d75855cad2138
gabriel.souza <gabriel.souza@GPA-TI-024.gabl.lan>
2018-02-22 16:08:26 -0300

    Mostrando o delete

--- TesteGit/src/br/abl/gitt/SalesData.java
+++ TesteGit/src/br/abl/gitt/SalesData.java
@@ -5,8 +5,7 @@ public class SalesData {
 	int data[] = {0,4,42};
 	
 	public void display() {
-		System.out.println("TODO: Implement some shit");
-		System.out.println("Eric");
+		System.out.println("AGORA É ESSE O PRINT");
 	}
 	
 }
